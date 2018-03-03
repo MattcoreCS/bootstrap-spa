c1783385ea8ea4f406e80f2b22787366abcc1398
Matheus Sartori <ms.sartori@outlook.com.br>
2018-03-02 21:45:17 -0300

    Release 1.0

--- js/navbar-animation-fix.js
+++ js/navbar-animation-fix.js
@@ -1,8 +1,9 @@
-$('#collapse-navbar').on('show.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
+$(function() {
+	$('#navbar').on('show.bs.collapse', function() {
+		$('.topCasaFina-banner').css('transform', 'translate(-50%, 10%');
+	});
 
-});
-
-$('#collapse-navbar').on('hide.bs.collapse', function() {
-	$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+	$('#navbar').on('hide.bs.collapse', function() {
+		$('.topCasaFina-banner').css('transform', 'translate(-50%, -50%');
+	});
 });
